a25c91d18cfb71d1e8b2502f6af17bce5e60626c
bitacademy-poscodx <poscoict.bitacademy@gmail.com>
2024-07-18 18:26:38 +0900

    1

--- tabbox/backend/src/main/resources/script/launch.sh
+++ tabbox/backend/src/main/resources/script/launch.sh
@@ -13,4 +13,4 @@ fi
 
 echo "starting [$APPLICATION_NAME]"
 cd $SCRIPT_DIR
-nohup java -Dspring.profiles.active=production -jar $SCRIPT_DIR/$APPLICATION_NAME.jar 1>log.out 2>err.out &
+nohup java -Dspring.profiles.active=production -jar $SCRIPT_DIR/$APPLICATION_NAME.jar 1>launch.log 2>launch.err.log &
