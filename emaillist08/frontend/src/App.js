b2289736144962c362750f553efdea9bedfd224b
bitacademy-poscodx <poscoict.bitacademy@gmail.com>
2024-07-18 11:49:10 +0900

    api #1

--- emaillist08/frontend/src/App.js
+++ emaillist08/frontend/src/App.js
@@ -1,36 +1,11 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import RegisterForm from './RegisterForm';
 import SearchBar from './SearchBar';
 import Emaillist from './Emaillist';
 import './assets/scss/App.scss';
 
 function App() {
-    const emails = [
-        {
-            "no": 1,
-            "firstName": "둘",
-            "lastName": "리",
-            "email": "dooly@gmail.com"
-        },
-        {
-            "no": 2,
-            "firstName": "마",
-            "lastName": "이콜",
-            "email": "michol@gmail.com"
-        },
-        {
-            "no": 3,
-            "firstName": "도",
-            "lastName": "우너",
-            "email": "douner@gmail.com"
-        },
-        {
-            "no": 4,
-            "firstName": "또",
-            "lastName": "치",
-            "email": "ddochi@gmail.com"
-        }
-    ];
+    const [emails, setEmails] = useState(null);
 
     return (
         <div id={'App'}>
